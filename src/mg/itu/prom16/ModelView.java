dc67d55fa6e60bc39509d368065681741d0bd8ce
Mercia04 <joarinesa@gmail.com>
2024-06-06 20:07:14 +0200

    finition sprint4-sprint5

---
+++ src/mg/itu/prom16/ModelView.java
@@ -0,0 +1,28 @@
+package mg.itu.prom16;
+
+import java.util.HashMap;
+
+public class ModelView {
+    private String url;
+    private HashMap<String, Object> data = new HashMap<>();
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public HashMap<String, Object> getData() {
+        return data;
+    }
+
+    public void setData(HashMap<String, Object> data) {
+        this.data = data;
+    }
+
+    public void addObject(String key, Object object) {
+        this.data.put(key, object);
+    }
+}
